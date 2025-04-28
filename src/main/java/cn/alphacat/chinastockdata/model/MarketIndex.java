e3a94407f343b0e648cfa55aba50e7f7c000c01f
CNLyon <alphacat1024@163.com>
2025-04-28 16:23:26 +0800

    check Valid  for MarketIndex version 1.0.7

--- src/main/java/cn/alphacat/chinastockdata/model/MarketIndex.java
+++ src/main/java/cn/alphacat/chinastockdata/model/MarketIndex.java
@@ -19,4 +19,38 @@ public class MarketIndex {
   private BigDecimal amount;
   private BigDecimal change;
   private BigDecimal changePct;
+
+  public boolean checkValid() {
+    if (this.indexCode == null || this.indexCode.isEmpty()) {
+      return false;
+    }
+    if (this.tradeDate == null) {
+      return false;
+    }
+    if (this.open == null) {
+      return false;
+    }
+    if (this.high == null) {
+      return false;
+    }
+    if (this.low == null) {
+      return false;
+    }
+    if (this.close == null) {
+      return false;
+    }
+    if (this.volume == null) {
+      return false;
+    }
+    if (this.amount == null) {
+      return false;
+    }
+    if (this.change == null) {
+      return false;
+    }
+    if (this.changePct == null) {
+      return false;
+    }
+    return true;
+  }
 }
